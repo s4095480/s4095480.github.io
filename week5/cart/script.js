d95b5f1ad7700b82b20da8d4f93d25d61bfbf220
s4095480 <167755482+s4095480@users.noreply.github.com>
2025-04-10 09:57:36 +1000

    Update script.js

--- week5/cart/script.js
+++ week5/cart/script.js
@@ -16,3 +16,19 @@ if (total > 100) {
   let discountedPrice = (total = total * discount);
   console.log("Discounted price is", discountedPrice);
 }
+
+//variables let and const
+// types of variables
+// numerical veriable let a = 0 -  + - * /
+// string/ text variables let name = "Name" 'name'
+// ` name
+// boolean variables true false let isItRaining = false;
+// objects have curly brackets
+// let myRecord = { name: "ak", id: 1, isLocal: false;}
+// myRecord.name = ak
+// arrays
+// let numbers =[2,4,6,8,10];
+// arrays start at 0 not 1
+// numbers[2] = 6
+// if condition
+// for loops
